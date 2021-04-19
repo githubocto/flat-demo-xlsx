4eed4f644ba89d322b848240d2a55c51f5310ec9
Irene Alvarado <irealva@users.noreply.github.com>
2021-04-19 09:25:00 -0700

    Update postprocess.ts

--- postprocess.ts
+++ postprocess.ts
@@ -2,13 +2,16 @@ import {
   xlsx,
   readXLSX,
   writeCSV,
-} from "https://deno.land/x/flat@0.0.4/mod.ts";
+} from "https://deno.land/x/flat@0.0.6/mod.ts";
 
 const inputFilename = Deno.args[0];
 const outputFilename = inputFilename.replace(".xlsx", ".csv");
 
 const workbook = await readXLSX(inputFilename);
+console.log(workbook)
 const sheetData = workbook.Sheets[workbook.SheetNames[2]];
+console.log(sheetData)
 const csvString = await xlsx.utils.sheet_to_csv(sheetData);
+console.log(csvString)
 
 await writeCSV(outputFilename, csvString);
